d48c9db8d6d835f1f9ae35c6af132a73133fa0af
Nikita <n.borovsky36@gmail.com>
2025-04-12 15:10:21 +0200

    Add react-scripts to fix build error3

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
         apiKey: "AIzaSyBuJn2lmxBoKpGwdfigyV7MMuiiVzdnZ60",
         authDomain: "nfc-site-90a68.firebaseapp.com",
         projectId: "nfc-site-90a68",
-        storageBucket: "nfc-site-90a68.firebasestorage.app",
+        storageBucket: "nfc-site-90a68.appspot.com",
         messagingSenderId: "11838911031",
         appId: "1:11838911031:web:f3c0f9a8906933d01245b2",
         measurementId: "G-30VTLTW4P7"
